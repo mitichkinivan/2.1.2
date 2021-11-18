afb4f175d343fbef8d11f85bcf45b459913a3611
mitichkinivan <mitichkin.ivan@gmail.com>
2021-11-18 23:21:11 +0700

    1.0

---
+++ src/main/java/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+    private String name;
+    private String breed;
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
